2091d3738a4d86ce406f12b5bc9f879169d94902
Samantha Konigsberg <skonigs@me.com>
2018-10-18 20:27:52 -0400

    privacy policy

---
+++ inVISABLE/PrivacyPolicyViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PrivacyPolicyViewController.swift
+//  inVISABLE
+//
+//  Created by Samantha Konigsberg on 10/18/18.
+//  Copyright © 2018 Samantha Konigsberg. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyPolicyViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
